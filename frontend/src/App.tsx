59fbbad7b31e490dd420f018f61f076902a5f87f
pinky <pinkipandarojo@gmail.com>
2025-07-03 18:45:31 -0400

    feat #774 Estructurar las carpetas del proyecto

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
-import Login from "./components/Login";
-import ModeSelector from "./components/ModeSelector";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ForgotPasswordPage from "./pages/auth/ForgotPasswordPage";
 import ResetPasswordPage from "./pages/auth/ResetPasswordPage";
 
 function App() {
-  const [mode, setMode] = useState("center");
 
   return (
     <div className="background-full">
