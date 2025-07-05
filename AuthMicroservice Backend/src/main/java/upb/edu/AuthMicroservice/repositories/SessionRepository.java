dfbb6ed0d4c88024c90c9258c19870909ef389a3
DiegoOrellana <diegoorellanaggch13@gmail.com>
2025-07-05 01:22:24 -0400

    Remueve del tracking archivos que ahora están en .gitignore

--- AuthMicroservice Backend/src/main/java/upb/edu/AuthMicroservice/repositories/SessionRepository.java
+++
@@ -1,9 +0,0 @@
-package upb.edu.AuthMicroservice.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import upb.edu.AuthMicroservice.models.Session;
-
-import java.util.UUID;
-
-public interface SessionRepository extends JpaRepository<Session, UUID> {
-}
--- AuthMicroservice Backend/src/main/java/upb/edu/AuthMicroservice/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package upb.edu.AuthMicroservice.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import upb.edu.AuthMicroservice.models.User;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
