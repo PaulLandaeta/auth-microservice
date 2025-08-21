7772ad0fea3fb8b8b56aff3efd59fcc07faad07b
joackagui <165275907+joackagui@users.noreply.github.com>
2025-08-20 23:35:40 -0400

    feat/#809/microservice-backend-deployment

--- AuthMicroservice Backend/src/main/java/upb/edu/AuthMicroservice/config/RedisConfig.java
+++ AuthMicroservice Backend/src/main/java/upb/edu/AuthMicroservice/config/RedisConfig.java
@@ -1,7 +1,9 @@
 package upb.edu.AuthMicroservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -9,9 +11,18 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${REDIS_HOST:localhost}")
+    private String redisHost;
+
+    @Value("${REDIS_PORT:6379}")
+    private int redisPort;
+
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("localhost", 6379);
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisHost);
+        config.setPort(redisPort);
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
